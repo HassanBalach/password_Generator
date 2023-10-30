21ec7610f56ff6a0cd65234ff751e50a870e6438
Hassan <islamicrule999@gmail.com>
2023-10-30 09:41:11 +0500

    hook variables are added in the code now

--- password_generator/src/App.jsx
+++ password_generator/src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [length, setLength] = useState(8);
+  const [numberAllow , setNumberAllow] = useState(fasle)
+  const [charAllow , setCharAllow] = useState(false)
+  const [password, setPassword] = useState('');
+  
 
   return (
     <>
