7dc2eea21ac20b7f3e1b84a22030bfd06c201d63
Hassan <islamicrule999@gmail.com>
2023-10-30 10:15:13 +0500

    Introduction to useCallback hook

--- password_generator/src/App.jsx
+++ password_generator/src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from "react"
-
-
+import { useCallback } from "react";
+import { useState } from "react";
 
 function App() {
-
   const [length, setLength] = useState(8);
-  const [numberAllow , setNumberAllow] = useState(fasle)
-  const [charAllow , setCharAllow] = useState(false)
-  const [password, setPassword] = useState('');
-  
+  const [numberAllow, setNumberAllow] = useState(fasle);
+  const [charAllow, setCharAllow] = useState(false);
+  const [password, setPassword] = useState("");
 
-  return (
-    <>
+  const passwordGenerater = useCallback(() => {
+    //  useCallback is function and defendences:
 
-    </>
-  )
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    //What is being happened in this code :str a (variable) is being called and add
+    // if the defendences are being true:
+
+    if (numberAllow) str += 1023456789;
+    if (charAllow) str += "!@#$%^&*(){}|~`";
+  }, [length, numberAllow, charAllow, password]); //Defendences are always written in array
+
+  return <></>;
 }
 
-export default App
+export default App;
